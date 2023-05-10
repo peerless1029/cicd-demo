658a1aee3c402c6dd38e969b562cf96e836d911a
Peerless1029 <172453591@qq.com>
2023-05-10 17:11:52 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>CICD-DEMO</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
